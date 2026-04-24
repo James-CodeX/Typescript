798efbfc0f3a24d2753e14b53309d103ee732081
James Karanja <124869546+James-CodeX@users.noreply.github.com>
2026-04-24 22:03:18 +0300

    Add shape-related types and functions for area calculation

--- 03PURETS/src/detection.ts
+++ 03PURETS/src/detection.ts
@@ -44,10 +44,73 @@ function isAdminAccount(account: User | Admin) {
     }
 }
 
+new Date()
+
 function logValue(x: Date | string) {
     if (x instanceof Date) {
         console.log(x.toUTCString());
     } else {
         console.log(x.toUpperCase());
     }   
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird) : pet is Fish {
+    return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+    if (isFish(pet)) {
+        pet
+        return "fish food";
+    } else {
+        pet
+        return "bird food";
+    }
+}
+
+
+
+interface Circle {
+    kind: "circle",
+    radius: number; 
+}
+
+interface Square {
+    kind: "square",
+    side: number;
+}
+
+interface Rectangle {
+    kind: "rectangle",
+    length: number;
+    width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    } else if (shape.kind === "square") {
+        return shape.side * shape.side;
+    } else if (shape.kind === "rectangle") {
+        return shape.length * shape.width;
+    }
+}
+
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape: never = shape;
+            return _defaultforshape;
+    }
 }
